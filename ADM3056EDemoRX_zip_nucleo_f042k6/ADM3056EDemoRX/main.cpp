abb085c741fc6fb6b25c045b9c788f2999c28c6e
OsciX <ryrutdavis@gmail.com>
2019-05-06 14:01:58 -0400

    Add code

---
+++ ADM3056EDemoRX_zip_nucleo_f042k6/ADM3056EDemoRX/main.cpp
@@ -0,0 +1,61 @@
+#include "CAN.h"
+#include "mbed.h"
+
+
+#define TXD_PIN PA_12
+#define RXD_PIN PA_11
+#define SILENT_PIN A2
+#define STBY_PIN A1
+#define RS_PIN A0
+
+DigitalOut SILENT(SILENT_PIN);
+DigitalOut STBY(STBY_PIN);
+DigitalOut RS(RS_PIN);
+
+#if defined (DEVICE_CAN) || defined(DOXYGEN_ONLY)
+
+Ticker ticker;
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+/** The constructor takes in RX, and TX pin respectively.
+  * These pins, for this example, are defined in mbed_app.json
+  */
+CAN can1(RXD_PIN, TXD_PIN);
+
+/*
+void send() {
+    printf("send()\n");
+    if(can1.write(CANMessage(1337, (char*) &data, 8))) {
+        printf("wloop()\n");
+        printf("Message sent: %d\n", data);
+        data++;
+    } 
+    led1 = !led1;
+}
+*/
+int main() {
+    
+    SILENT = 0;
+    STBY = 0;
+    RS = 0;
+    
+    can1.frequency(125000);
+    can1.mode(CAN::Normal);
+    //printf("main()\n");
+    //ticker.attach(&send, 1);
+    CANMessage msg;
+    while(1) {
+        //printf("loop()\n");
+        printf("waiting");
+        if(can1.read(msg)) {
+            printf("Message received: %d\n", msg.data[0]);
+            led2 = !led2;
+        } 
+        wait(1);
+    }
+}
+
+#else
+  #error CAN NOT SUPPORTED
+  
+#endif
